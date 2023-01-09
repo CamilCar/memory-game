5ec9eb3388fe901a947892d745fd279c1ff9712a
CamilCar <cc.carsbo@gmail.com>
2023-01-09 20:19:34 +0000

    Add function in js to flip card

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,9 @@ img.style = {
 }; 
 img.src = 'assets/images/buckingham-palace.jpeg';
 document.getElementsByClassName("card-front")[0].appendChild(img);
+
+function flip(event) {
+    let currentTarget = event.currentTarget;
+    currentTarget.children[1].style.visibility = "hidden";
+
+}

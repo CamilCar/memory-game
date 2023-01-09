f27fd869ada6299b97cd80d928c409d64e5931fc
CamilCar <cc.carsbo@gmail.com>
2023-01-09 20:44:44 +0000

    add loop in js for card pictures

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,25 @@
+/* initiate game */
+imagesToBoard();
 
-let img = document.createElement('img');
-img.style = {
 
-  height: '10%',
-  width: '10%'
-}; 
-img.src = 'assets/images/buckingham-palace.jpeg';
-document.getElementsByClassName("card-front")[0].appendChild(img);
+/* images */ 
+
+function imagesToBoard() {
+    let imageArray = [
+        'assets/images/buckingham-palace.jpeg',
+        'assets/images/colosseum.jpeg',
+        'assets/images/eiffel-tower.jpeg',
+        'assets/images/notre-dame.jpeg',
+        'assets/images/taj-mahal.jpeg',
+        'assets/images/white-house.jpeg',
+    ]
+    for(let i = 0; 0 < imageArray.length ; i++){
+        let img = document.createElement('img');
+        img.src = imageArray[i];
+        document.getElementsByClassName("card-front")[i].appendChild(img);
+    }
+}
+
 
 function flip(event) {
     let currentTarget = event.currentTarget;

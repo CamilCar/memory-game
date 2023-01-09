6db18828b66373650db6abd1a26875f4bf8b7578
CamilCar <cc.carsbo@gmail.com>
2023-01-09 18:41:32 +0000

    Add first img in js and add styles for it in css

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+
+let img = document.createElement('img');
+img.style = {
+
+  height: '10%',
+  width: '10%'
+}; 
+img.src = 'assets/images/buckingham-palace.jpeg';
+document.getElementsByClassName("card-front")[0].appendChild(img);
